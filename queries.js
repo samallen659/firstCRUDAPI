e6a643fb141224051fb76ba4808736944b062b44
Sam Allen <samallen659@gmail.com>
2023-04-26 20:27:29 +0100

    added boilerplate postgres connection

---
+++ queries.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
